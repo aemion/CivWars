3b9f5d35050e44a70cc801355cc55cc790e21e23
Alexandre Emion <alexandre.emion@gmail.com>
2015-12-13 20:16:39 +0100

    Début implémentation game entity

--- dev/src/fr/emion/civwars/shared/model/entities/GameEntity.java
+++ dev/src/fr/emion/civwars/shared/model/entities/GameEntity.java
@@ -1,5 +1,62 @@
 package fr.emion.civwars.shared.model.entities;
 
-public abstract class GameEntity {
+import java.util.LinkedList;
 
+import fr.emion.civwars.shared.model.orders.Order;
+
+public abstract class GameEntity {
+  private int id;
+  private static int ID_GENERATOR = 0; // Mb a method to change that to sync with the server
+  private LinkedList<Order> orders;
+  
+  /**
+   * Constructs a game entity with a unique id and no orders.
+   */
+  public GameEntity() {
+    id = ID_GENERATOR++;
+    orders = new LinkedList<Order>(); 
+  }
+
+  /**
+   * Returns the id of the game entity.
+   * @return the id of the game entity.
+   */
+  public int getId() {
+    return id;
+  }
+
+  
+  /**
+   * Queues a new order for the game entity.
+   * @param o The order to be queued.
+   */
+  public void queueOrder(Order o) {
+    orders.add(o);
+  }
+  
+  /**
+   * Clears all the orders in the queue of the game entity.
+   */
+  public void removeQueuedOrders() {
+    orders.clear();
+  }
+  
+  /**
+   * Executes the current order.
+   * @see Order#execute()
+   */
+  public void execute() {
+    if(getCurrentOrder() != null) 
+      if(getCurrentOrder().execute())
+        goToNextOrder();
+  }
+  
+
+  private void goToNextOrder() {
+    orders.poll();
+  }
+
+  private Order getCurrentOrder() {
+    return orders.peekFirst();
+  }
 }
--- dev/src/fr/emion/civwars/shared/model/orders/Order.java
+++ dev/src/fr/emion/civwars/shared/model/orders/Order.java
@@ -1,5 +1,10 @@
 package fr.emion.civwars.shared.model.orders;
 
 public abstract class Order {
+  /**
+   * Executes the order. The order can be executed in one or in several times. If the order is finished, this method will return true.
+   * @return true if the order has finished executing, false otherwise.
+   */
+  public abstract boolean execute();
 
 }
