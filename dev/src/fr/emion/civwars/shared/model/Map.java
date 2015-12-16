a63c9cf471b4044e84adb6ae46523ec81653ab98
Alexandre Emion <alexandre.emion@gmail.com>
2015-12-16 12:59:10 +0100

    Impl methodes Map

--- dev/src/fr/emion/civwars/shared/model/Map.java
+++ dev/src/fr/emion/civwars/shared/model/Map.java
@@ -1,5 +1,23 @@
 package fr.emion.civwars.shared.model;
 
-public class Map {
+import java.util.LinkedList;
 
+import fr.emion.civwars.shared.model.entities.GameEntity;
+
+public class Map {
+  private LinkedList<GameEntity> entities;
+  
+  public Map() {
+    entities = new LinkedList<GameEntity>();
+  }
+  
+  public void executeOneTurn() {
+    for(GameEntity e : entities)
+      e.execute();
+  }
+  
+  public void addGameEntity(GameEntity e) {
+    entities.add(e);
+  }
+  
 }
